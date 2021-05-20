3c17122f3705e704d525eee639143e83d56e1498
Kevin Eddie <keddie024@gmail.com>
2021-05-20 18:52:32 -0400

    created basic component structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <Wrapper>
+      <Header />
+      <Main />
+    </Wrapper>
+  );
+}
+
+export default App;
