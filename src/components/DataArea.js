bb246e5be34773cfba50e1c2cd3c25f0c21835ed
Kevin Eddie <keddie024@gmail.com>
2021-05-24 15:10:41 -0400

    render area now displays data

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,40 +1,63 @@
 import React, { Component } from "react";
-import SearchForm from "./SearchForm";
-import ResultList from "./ResultList";
-import API from "../utils/API";
 
 class DataArea extends Component {
   state = {
     employees: [],
+    sortBy: null
   };
 
   componentDidMount() {
     fetch("https://randomuser.me/api/?results=20")
-    .then(res => res.json())
-    .then(json => {
-      const employees = json.results.map(employee => {
-        return {
-          firstName: employee.name.first,
-          lastName: employee.name.last,
-          img: employee.picture.thumbnail
-        };
-      });
-      this.setState({employees});
-    })
+      .then(res => res.json())
+      .then(json => {
+        const employees = json.results.map(employee => {
+          return {
+            firstName: employee.name.first,
+            lastName: employee.name.last,
+            gender: employee.gender,
+            img: employee.picture.thumbnail
+          };
+        });
+        this.setState({ employees });
+      })
   }
 
-  sortFirstName () {
+  sortFirstName = () => {
     let employees = [...this.state.employees];
 
-    employees.sort((a,b) => a.firstName.localeCompare(b.firstName));
-    this.setState({employees});
+    employees.sort((a, b) => a.firstName.localeCompare(b.firstName));
+    this.setState({ employees });
   }
 
-  sortLastName () {
+  sortLastName = () => {
     let employees = [...this.state.employees];
 
-    employees.sort((a,b) => a.lastName.localeCompare(b.lastName));
-    this.setState({employees});
+    employees.sort((a, b) => a.lastName.localeCompare(b.lastName));
+    this.setState({ employees });
+  }
+
+  render() {
+    if (!this.state.employees.length) {
+      return <div>No employees found!</div>
+    }
+
+    return (
+      <div>
+        <button onClick={this.sortFirstName}>Sort by First Name</button>
+        <button onClick={this.sortLastName}>Sort by Last Name</button>
+
+        <table>
+          <tr><th></th><th>Name:</th><th>Gender:</th></tr>
+          {this.state.employees.map(employee => (
+            <tr>
+              <td><img src={employee.img} alt={employee.firstName} /></td>
+              <td>{employee.firstName} {employee.lastName}</td>
+              <td>{employee.gender}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    )
   }
 
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../styles/Header.css";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Employee Directory</h2>
+            </div>
+        );
+    }
+}
